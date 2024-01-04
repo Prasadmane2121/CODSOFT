111e4cce54abe7bf21dc5b2d2a8259c1029cbc8a
Prasadmane2121 <109418644+Prasadmane2121@users.noreply.github.com>
2024-01-04 13:32:16 +0530

    Adding calculator project

--- CALCULATOR/script.js
+++ CALCULATOR/script.js
@@ -1,17 +1,44 @@
-let display = document.getElementById('display');
+let currentValue = '';
+    let currentOperation = '';
+    let displayedValue = document.getElementById('display');
 
-function appendToDisplay(value) {
-  display.value += value;
-}
+    function appendValue(value) {
+        currentValue += value;
+        displayedValue.value = currentValue;
+    }
 
-function clearDisplay() {
-  display.value = '';
-}
+    function setOperation(operation) {
+        currentOperation = operation;
+        currentValue += ' ' + operation + ' ';
+        displayedValue.value = currentValue;
+    }
 
-function calculateResult() {
-  try {
-    display.value = eval(display.value);
-  } catch (error) {
-    display.value = 'Error';
-  }
-}
+    function clearDisplay() {
+        currentValue = '';
+        displayedValue.value = '';
+    }
+
+    function calculateResult() {
+        currentValue = eval(currentValue);
+        displayedValue.value = "= "+ currentValue;
+    }
+    function backspace() {
+        currentValue = currentValue.slice(0, -1);
+        displayedValue.value = currentValue;
+    }
+
+    document.addEventListener('keydown', function(event) {
+        const key = event.key;
+
+        if (/[0-9]/.test(key)) {
+            appendValue(key);
+        } else if (key === '+' || key === '-' || key === '*' || key === '/') {
+            setOperation(key);
+        } else if (key === 'Enter') {
+            calculateResult();
+        } else if (key === 'Escape') {
+            clearDisplay();
+        } else if (key === 'Backspace') {
+            backspace();
+        }
+    });               
